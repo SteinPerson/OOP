0600d4afde0ba3db9e831efa21529956d95f0d06
Daniil <steinf121@gmail.com>
2020-11-13 14:02:47 +0200

    first commit

---
+++ Lab2/Lab2_Fainshtein/Lab2_Fainshtein/Ellipse.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Math;
+
+namespace Lab2_Fainshtein
+{
+    class Ellipse
+    {
+        double smallSemiAxis, bigSemiAxis;
+
+        // Конструктор
+        public Ellipse()
+        {
+            smallSemiAxis = 10;
+            bigSemiAxis = 20;
+        }
+
+        // Деструктор
+        ~Ellipse()
+        {
+            Console.WriteLine("Эллипс удален");
+        }
+
+        // Площадь
+        public double Area()
+        {
+            double area;
+            area = PI * smallSemiAxis * bigSemiAxis;
+            return area;
+        }
+
+        // Геттеры и Сеттеры
+        public void set_smallSemiAxis(double sA)
+        {
+            smallSemiAxis = sA;
+        }
+        public void set_bigSemiAxis(double sB)
+        {
+            bigSemiAxis = sB;
+        }
+        public double get_smallSemiAxis()
+        {
+            return smallSemiAxis;
+        }
+        public double get_bigSemiAxis()
+        {
+            return bigSemiAxis;
+        }
+    }
+}
---
+++ Lab2/Lab2_Fainshtein/Lab2_Fainshtein/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lab2_Fainshtein
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char exit = 'n'; // Выйти ли из программы
+
+            do
+            {
+                bool ifCorrect = true; // Правильно ли введено число
+                double ellipseArea;
+                double bigSAxis = 0, smallSAxis = 0;
+                Ellipse ellipse = new Ellipse();
+
+                // Задавание размеров
+                do
+                {
+                    try
+                    {
+                        ifCorrect = true;
+                        Console.WriteLine("Введите рамзер большей полуоси:");
+                        bigSAxis = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Введите рамзер меньшей полуоси:");
+                        smallSAxis = Convert.ToDouble(Console.ReadLine());
+                    } catch (FormatException)
+                    {
+                        ifCorrect = false;
+                        Console.WriteLine("Вводить можно только цифры! Попробуйте еще раз");
+                        Thread.Sleep(1000);
+                        continue;
+                    }
+                } while (!ifCorrect);
+
+                ellipse.set_bigSemiAxis(bigSAxis);
+                ellipse.set_smallSemiAxis(smallSAxis);
+                ellipseArea = ellipse.Area(); // Площадь
+
+                Console.WriteLine("Площадь эллипса: " + ellipseArea);
+
+                // Выйти ли
+                do
+                {
+                    Console.WriteLine("Если хотите выйти из программы напишите \"y\", если нет, то \"n\"");
+                    // Запрет ввода не букв
+                    try
+                    {
+                        exit = Convert.ToChar(Console.ReadLine());
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("Неправильно введено значение! Попробуйте еще раз");
+                        Thread.Sleep(1000);
+                        exit = 'q';
+                        continue;
+                    }
+                    // Запрет ввода не латинских y и n
+                    if (exit != 'n' && exit != 'y')
+                    {
+                        Console.WriteLine("Неправильно введено значение! Попробуйте еще раз");
+                        Thread.Sleep(1000);
+                    }
+                } while (exit != 'n' && exit != 'y');
+
+            } while (exit == 'n'); // Выход из программы при значении 'y'
+        }
+    }
+}
